006c5be7aede52eadc366ca1effc3df849b07699
lucasadlerstein <lucasadlerstein@gmail.com>
2020-10-23 09:26:49 -0300

    first commit

---
+++ src/pages/quiz/index.js
@@ -0,0 +1,18 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+
+import Layout from "../../components/layout"
+
+import SEO from "../../components/seo"
+
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+  </Layout>
+)
+
+export default IndexPage
